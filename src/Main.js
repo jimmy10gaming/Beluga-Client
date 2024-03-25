49317973e46f1ebd9302a5b92a6a81a4c8d972f0
(AOD)GG{LEADER} <114262675+AODGGLEADER@users.noreply.github.com>
2024-03-25 23:14:44 +0000

    eeeeee

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,15 @@
 const { CreateMenu } = require("./GUI/Menu");
 const { StartupModules } = require("./GUI/Modules");
 
+const clientName = "Fracticle Client";
+const clientVersion = "v1.0.0";
+
+const logger = ModAPI.logger;
+logger.setLogger({ name: clientName });
+logger.logInfo({ string: `Starting ${clientName} ${clientVersion}...` });
+
+document.title = `${clientVersion} ${clientName}`;
+
 StartupModules();
 
 window.addEventListener("keydown", function (e) {
