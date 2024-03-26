d89e19e033ad375e2806748c7ae7219c269be284
(AOD)GG{LEADER} <114262675+AODGGLEADER@users.noreply.github.com>
2024-03-26 17:12:57 +0000

    hehehehar

--- src/GUI/Modules.js
+++ src/GUI/Modules.js
@@ -25,52 +25,51 @@ export function SetupModules() {
     const Module = document.createElement("div");
 
     Module.innerHTML = `
-      <div style="display: flex; flex-direction: column; align-items: center; justify-content: end; height: 25vh; width: 25vh; border-radius: 2.5vh; background-color: rgba(255, 255, 255, 0.15); border: solid 0.1vw rgba(255, 255, 255, 0.5); position: relative;">
+        <div style="display: flex; flex-direction: column; align-items: center; justify-content: end; height: 25vh; width: 25vh; border-radius: 2.5vh; background-color: rgba(255, 255, 255, 0.15); border: solid 0.1vw rgba(255, 255, 255, 0.5); position: relative;">
             <img style="position: absolute; top: 35%; left: 50%; transform: translate(-50%, -50%); font-size: 2vh; width: 15vh; height: 15vh;" src="${img}"/>
             <h1 style="word-wrap: break-word;position: absolute; top: 62.5%; left: 50%; transform: translate(-50%, -50%); font-size: 1.5vh; width: 100%; text-align: center;">${name}</h1>
             <a style="background-color: rgba(255, 0, 0, 0.25); width: 100%; height: 20%; bottom: 0; border-radius: 0 0 2.5vh 2.5vh; position: relative; border: solid 0.1vw rgba(255, 255, 255, 0.5);" id="SCMM-${name}-Toggle">
                 <p style="position: absolute; top: 10%; left: 50%; transform: translate(-50%, -50%); font-size: 2vh;">DISABLED</p>
             </a>
         </div>
-      `;
+    `;
 
     const Holder = document.getElementById("SCMM-MODULES");
     if (Holder) Holder.appendChild(Module);
-  }
+}
 
-  function EnableToggle(name) {
+function EnableToggle(name) {
     document.getElementById(`SCMM-${name}-Toggle`).addEventListener("mousedown", function (e) {
-      const i = ParsedLSData.findIndex((e) => e.name === name);
+        const i = ParsedLSData.findIndex((e) => e.name === name);
 
-      if (i !== -1) {
-        ParsedLSData[i].enabled = !ParsedLSData[i].enabled;
-        eval(`${name}.Init("${name}")`);
+        if (i !== -1) {
+            ParsedLSData[i].enabled = !ParsedLSData[i].enabled;
+            eval(`${name}.Init("${name}")`);
 
-        localStorage.setItem("SCMM-MODS", JSON.stringify(ParsedLSData));
-      }
+            localStorage.setItem("SCMM-MODS", JSON.stringify(ParsedLSData));
+        }
     })
-  }
+}
 
-  function SetCorrectToggle(name) {
+function SetCorrectToggle(name) {
     const i = ParsedLSData.findIndex((e) => e.name === name);
 
     if (i !== -1) {
-      const Toggle = document.getElementById(`SCMM-${name}-Toggle`);
-      if (Toggle) {
-        Toggle.style.backgroundColor = ParsedLSData[i].enabled ? "rgba(0, 255, 0, 0.25)" : "rgba(255, 0, 0, 0.25)";
-        Toggle.querySelector("p").innerHTML = ParsedLSData[i].enabled ? "ENABLED" : "DISABLED";
-      }
+        const Toggle = document.getElementById(`SCMM-${name}-Toggle`);
+        if (Toggle) {
+            Toggle.style.backgroundColor = ParsedLSData[i].enabled ? "rgba(0, 255, 0, 0.25)" : "rgba(255, 0, 0, 0.25)";
+            Toggle.querySelector("p").innerHTML = ParsedLSData[i].enabled ? "ENABLED" : "DISABLED";
+        }
     }
-  }
+}
 
-  ModulesList.forEach((Module) => {
+ModulesList.forEach((Module) => {
     BareLocalStorageData = localStorage.getItem("SCMM-MODS");
     ParsedLSData = JSON.parse(BareLocalStorageData);
-  
+
     Add(Module.name, Module.imagedata);
     EnableToggle(Module.name);
     setInterval(function () {
-      SetCorrectToggle(Module.name);
+        SetCorrectToggle(Module.name);
     }, 100);
-  });
-}
+})};
