ee2b5c8afd4795f8a19128e2358d5a170f847ff8
(AOD)GG{LEADER} <114262675+AODGGLEADER@users.noreply.github.com>
2024-03-26 01:01:25 +0000

    pp

--- src/Modules/Fullbright.js
+++ src/Modules/Fullbright.js
@@ -1,1 +1,21 @@
-print("helolol");
+let int;
+
+export function FullbrightOn() {
+  clearInterval(int);
+  int = setInterval(function () {
+    ModAPI.blocks.air.lightValue = 10;
+    ModAPI.blocks.water.lightValue = 10;
+    ModAPI.blocks.air.reload();
+    ModAPI.blocks.water.reload();
+  }, 5000);
+}
+
+export function FullbrightOff() {
+  clearInterval(int);
+  int = setInterval(function () {
+    ModAPI.blocks.air.lightValue = 1;
+    ModAPI.blocks.water.lightValue = 1;
+    ModAPI.blocks.air.reload();
+    ModAPI.blocks.water.reload();
+  }, 5000);
+}
