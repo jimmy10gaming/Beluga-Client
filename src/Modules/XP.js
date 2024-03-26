78cb4d03831f896b0fc42df384b6c04ae14ffde7
(AOD)GG{LEADER} <114262675+AODGGLEADER@users.noreply.github.com>
2024-03-26 09:02:49 -0700

    Update and rename CPS.js to XP.js and erm edited

---
+++ src/Modules/XP.js
@@ -0,0 +1,44 @@
+ModAPI.require("player");
+
+let XP = {};
+let int;
+
+eXPort function Init(name) {
+    const Data = JSON.parse(localStorage.getItem("SCMM-MODS"));
+    const ModuleIndex = Data.findIndex((e) => e.name === name);
+    const XP = document.getElementById("SCMM-XP");
+
+
+    if (ModuleIndex !== 1 && !Data[ModuleIndex].enabled) {
+        //start of the "enabled" stuff
+        int = setInterval(() => {
+      if (document.getElementById("SCMM-Keystrokes")) {
+        const XP = ModAPI.player.eXPerienceTotal();
+           XP.forEach((XP) => {
+          const element = document.getElementById(`${XP}XP`);
+          if (element && XP[XP]) {
+            element.style.color = "black";
+            element.style.background = "rgba(255, 255, 255, 0.5)";
+          } else {
+            element.style.color = "white";
+            element.style.background = "rgba(0, 0, 0, 0.5)";
+          }
+        });
+      }
+    }, 10);
+
+    const XP = document.createElement("div");
+        XP.id = "SCMM-XP"
+        XP.style = `font-size: 4vh;transform: translate(200px, -173px);display: grid;width: fit-content;height: fit-content;position: absolute;right: 0;bottom: 0;grid-template-areas: ". XP ." "Percent Needed";gap: 5px;`;
+        XP.innerHTML = `<div id="XPMaxAndTotal" style="border-radius: 1vh;min-height: 6vh;min-width: 6vh;display: flex;justify-content: center;align-items: center;font-family: 'Minecraftia';grid-area: XP;">XP amount/XP Total</div>
+        <div id="Percent" style="border-radius: 1vh;min-height: 6vh;min-width: 6vh;display: flex;justify-content: center;align-items: center;font-family: 'Minecraftia';grid-area: Percent;">Percent</div>
+        <div id="Needed" style="border-radius: 1vh;min-height: 6vh;min-width: 6vh;display: flex;justify-content: center;align-items: center;font-family: 'Minecraftia';grid-area: Needed;">Needed</div>`
+    document.body.appendChild(XP);
+        //end of the Endabled stuff
+    } else {
+        //start of Disabled code
+     if (int) clearInterval(int);
+    if (XP) XP.remove();        
+    //end of disabled code 
+    }
+}
