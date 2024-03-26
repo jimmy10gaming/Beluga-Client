6dbaa25d44a08f544bea1dc615463a54a37567e8
(AOD)GG{LEADER} <114262675+AODGGLEADER@users.noreply.github.com>
2024-03-26 16:07:11 +0000

    seeing if my new exp mod works lmfao, i am compleate garbadge at this new custom API :sob:

--- src/GUI/Modules.js
+++ src/GUI/Modules.js
@@ -1,7 +1,7 @@
 const { ModulesList } = require("../ModulesList");
 const Keystrokes = require("../Modules/Keystrokes"); 
 const Fullbright = require("../Modules/Fullbright");
-const CPS = require("../Modules/CPS");
+const CPS = require("../Modules/XpHud");
 // IF YOU ADD YOUR OWN MAKE SURE TO REQUIRE THEM HOW YOU NAMED IT IN THE "ModulesList.js" FILE!!!
 // Example: const CPS = require("../Modules/scriptnamecanbewhatever")
 
