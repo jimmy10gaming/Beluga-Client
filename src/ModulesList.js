14332f87fc75e75524e5b0f1a660214a3d6d98d8
(AOD)GG{LEADER} <114262675+AODGGLEADER@users.noreply.github.com>
2024-03-29 16:44:12 +0000

    e

--- src/ModulesList.js
+++ src/ModulesList.js
@@ -4,6 +4,7 @@ import Fullbrightimg from "./Base64/Fullbright.png"
 import XpHudimg from "./Base64/XpHud.png"
 import MangaFontimg from "./Base64/MangaFont.png"
 import ToggleSprintimg from "./Base64/ToggleSprint.png"
+import ChatShortcutsimg from "./Base64/ChatShortcuts.png"
 
 export const LogoData = Logo;
 
@@ -27,5 +28,9 @@ export let ModulesList = [
   {
     name: "ToggleSprint",
     imagedata: ToggleSprintimg,
+  },
+  {
+    name: "ChatShortcuts",
+    imagedata: ChatShortcutsimg,
   },
 ];
