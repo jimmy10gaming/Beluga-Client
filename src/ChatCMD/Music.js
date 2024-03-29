8adc2c54fb038133a5d08ea3e4f39c618780293c
(AOD)GG{LEADER} <114262675+AODGGLEADER@users.noreply.github.com>
2024-03-29 23:51:09 +0000

    :) my plan went wrong aggain fr :p

--- src/ChatCMD/Music.js
+++ src/ChatCMD/Music.js
@@ -1,28 +1,21 @@
 ModAPI.require('player')
 
-let songplayer = new Audio()
+let songplayer = new Audio('https://files.catbox.moe/k4j25x.mp3')
 songplayer.volume = 0.1
 
 ModAPI.addEventListener('sendchatmessage', function(e) {
-    const command = e.message.split(" ");
-    if (command[0] === '.play') {
+    if (e.message == '.play') {
         e.preventDefault = true
-        if (command.length >= 3) {
-            const service = command[1].toLowerCase();
-            const link = command.slice(2).join(" ");
-            playMusic(service, link);
-        } else {
-            ModAPI.displayToChat({msg: '§6[§4ERROR§6] §cPlease specify a valid service (YouTube or Spotify)'});
-        }
+        songplayer.play();
+        ModAPI.displayToChat({msg: '§3Now playing lo-fi'})
     } else if (e.message == '.pause') {
         e.preventDefault = true
         songplayer.pause();
-        ModAPI.displayToChat({msg: '§3Music paused'});
+        ModAPI.displayToChat({msg: '§3Lo-fi paused'})
     } else if (e.message == '.replay') {
         e.preventDefault = true
         songplayer.load();
-        songplayer.play();
-        ModAPI.displayToChat({msg: '§3Music replayed'});
+        ModAPI.displayToChat({msg: '§3Replaying lo-fi'})
     } else if (e.message.startsWith('.volume ')) {
         e.preventDefault = true
         try {
@@ -33,21 +26,3 @@ ModAPI.addEventListener('sendchatmessage', function(e) {
         }
     }
 })
-
-function playMusic(service, url) {
-    if (service === "youtube") {
-        // Use youtube-dl or similar library to download the audio from the YouTube link
-        // Then set the downloaded audio as the source for the songplayer
-        // Example:
-        // songplayer.src = "https://www.example.com/downloaded-audio.mp3";
-        ModAPI.displayToChat({msg: '§3Now playing music from YouTube'});
-    } else if (service === "spotify") {
-// Use a Spotify API to access the playlist and retrieve the tracks
-        // Then set one of the tracks as the source for the songplayer
-        // Example:
-        // songplayer.src = "https://www.example.com/spotify-track.mp3";
-        ModAPI.displayToChat({msg: '§3Now playing music from Spotify'});
-    } else {
-        ModAPI.displayToChat({msg: '§6[§4ERROR§6] §cPlease specify a valid service (YouTube or Spotify)'});
-    }
-}
