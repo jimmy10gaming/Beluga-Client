f119c4115938e69f2119f07380f9684ef7feeda4
(AOD)GG{LEADER} <114262675+AODGGLEADER@users.noreply.github.com>
2024-03-29 06:13:41 +0000

    more builds :p

--- src/Modules/XpHud.js
+++ src/Modules/XpHud.js
@@ -3,27 +3,23 @@ export function Init(name) {
     const ModuleIndex = Data.findIndex((e) => e.name === name);
 
     if (ModuleIndex !== 1 && !Data[ModuleIndex].enabled) {
+        
         ModAPI.require("player");
 
-        // Display a message in the chat when the mod is enabled
-        ModAPI.displayToChat({
-            msg: "Death Position Enabled"
-        });
+        ModAPI.displayToChat({ msg: "Death Position Enabled" });
 
-        // Listen for the playerDeath event
-        ModAPI.addEventListener("playerDeath", () => {
-            // Get the player's coordinates
+        ModAPI.addEventListener("playerdeath", () => {
             const player = ModAPI.player;
             const playerPos = player.getPositionVector();
             const x = Math.round(playerPos.x);
             const y = Math.round(playerPos.y);
             const z = Math.round(playerPos.z);
 
-            // Display the coordinates in the chat
             ModAPI.displayToChat({
                 msg: `You died at coordinates: X: ${x}, Y: ${y}, Z: ${z}`
             });
         });
+        
     } else {
         console.log("Disabled Mod");
     }
