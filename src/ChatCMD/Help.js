300cb3fe9361a01b570bbbaba067b462442006eb
(AOD)GG{LEADER} <114262675+AODGGLEADER@users.noreply.github.com>
2024-03-30 05:58:52 +0000

    awd

--- src/ChatCMD/Help.js
+++ src/ChatCMD/Help.js
@@ -8,11 +8,11 @@ ModAPI.addEventListener('sendchatmessage', function(m) {
 §6[-COMMANDS-]
 §6[Music-Controlls]
 §3.help §6\| §aDisplays this help dialogue
-§3.play {service} {url} §6\| §aPlays lo-fi (19 minutes long)
-services are Youtube or spotify
+§3.play §6\| §aPlays the song
 §3.pause §6\| §aPauses the lo-fi
 §3.replay §6\| §aReplays the lo-fi
 §3.volume §b[int] §6\| §aSets the volume of the lo-fi (max is 100)
+§3.setsong §b[url] §6\| §aAllows you to set the custom url to a new song
 §0==================================
         `})
          }
--- src/ChatCMD/Music.js
+++ src/ChatCMD/Music.js
@@ -2,6 +2,8 @@ ModAPI.require('player')
 
 let songplayer = new Audio('https://files.catbox.moe/k4j25x.mp3')
 songplayer.volume = 0.1
+let oldVolume = songplayer.volume;
+let loopToggle = false()
 
 ModAPI.addEventListener('sendchatmessage', function(e) {
     if (e.message == '.play') {
@@ -20,9 +22,21 @@ ModAPI.addEventListener('sendchatmessage', function(e) {
         e.preventDefault = true
         try {
             songplayer.volume = (e.message.substr(8) / 100)
-            ModAPI.displayToChat({msg: '§3Volume set to '+ (e.message.substr(8) / 100)})
+            oldVolume = songplayer.volume
+            ModAPI.displayToChat({msg: '§3Volume set to '+ e.message.substr(8)})
         } catch (error) {
             ModAPI.displayToChat({msg: "§6[§4ERROR§6] §c"+error})
         }
+    } else if (e.message.startsWith('.setsong ') && e.message.substr(9).startsWith('https://')) {
+        e.preventDefault = true
+        songplayer.pause()
+        songplayer = new Audio(e.message.substr(9))
+        songplayer.volume = oldVolume
+        ModAPI.displayToChat({msg: '§3URL was set to §6[ §b' + e.message.substr(9) + ' §6]'})
+    } else if (e.message == '.loop') {
+        e.preventDefault = true
+        songplayer.loop = loopToggle
+        loopToggle = !loopToggle
+        ModAPI.displayToChat({msg: '§3Loop is now set to §6[ §a'+loopToggle+' §6]'})
     }
 })
