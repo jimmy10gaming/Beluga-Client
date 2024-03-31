b038dab04aefac9e116f1c3e60c0f17938b58b5d
(AOD)GG{LEADER} <114262675+AODGGLEADER@users.noreply.github.com>
2024-03-31 17:08:48 +0000

    pogg

--- out/solar-client.js
+++
@@ -1,3 +0,0 @@
-var script = document.createElement('script');
-script.src = 'https://raw.githubusercontent.com/AODGGLEADER/Solar-Client/main/out/Fracticle-client.js';
-document.head.appendChild(script);
